32723f4b184ebee63124f5644042daa05829b3be
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2021-03-05 18:19:26 +0200

    start markup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>кафе Expresso</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Cafe Expresso</h1>
+        <h2>Please leave feedback</h2>
+        <div>
+          <button>Good</button>
+          <button>Neutral</button>
+          <button>Dad</button>
+        </div>
+        <div>
+          <ul>
+            <li>Good:</li>
+            <li>Neutral:</li>
+            <li>Dad:</li>
+            <li>Total:</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
